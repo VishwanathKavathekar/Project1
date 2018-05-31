6152973184cf90a66553bc4f374fcbf250967947
Vishwanath <Vishwanath@vishwanath>
2018-05-31 09:26:50 +0530

    yaay my first git program

---
+++ Bank/src/bank/Account.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bank;
+
+
+
+public class Account {
+   /**
+     * @param balance balance in the account
+     * @param name Name of account holder
+     */
+    String name;
+    int tot_bal;
+    /**
+     * Constructor to initialize the account name and balance
+     */
+    Account()
+    {
+        this.name=null;
+        this.tot_bal=0;
+    }
+}
